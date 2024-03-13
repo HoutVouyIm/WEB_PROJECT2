270925553dab31cc55376224819eab801ee3ba9c
HoutVouyIm <vouyimhout0962422747@gmail.com>
2024-03-13 09:36:46 +0700

    update

--- Auth/login.php
+++ Auth/login.php
@@ -3,9 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
+    <link rel="stylesheet" href="auth_style.css">
 </head>
 <body>
-    <h1>Login</h1>
+    <div class="login_box">
+        <h1>Login account</h1>
+        <form action="#" method="post">
+            <label>E-mail</label>
+            <input type="email" name="email" placeholder="123@gmail.com">
+
+            <label>Password</label>
+            <input type="password" name="password">
+
+            <input type="button" value="Login">
+        </form>
+        <p class="para-2">Not have account yet ? <a href="register.php">Sign Up</a></p>
+    </div>
+  
 </body>
 </html>
--- Auth/register.php
+++ Auth/register.php
@@ -3,9 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Register</title>
+    <link rel="stylesheet" href="auth_style.css">
 </head>
 <body>
-    <h1>Register</h1>
+    <div class="signup_box">
+ 
+        <h1>Create an account</h1>
+        <h4>To continue, fill out your personal info</h4>
+
+        <form action="#" method="post">
+            <label>Full Name: </label>
+            <input type="text" name="fullname" placeholder="Full Name">
+
+            <label>User Name</label>
+            <input type="text" name="username" placeholder="User Name">
+
+            <label>E-mail</label>
+            <input type="email" name="email" placeholder="123@gmail.com">
+
+            <label>Password</label>
+            <input type="password" name="password">
+
+            <label>Repeat Password</label>
+            <input type="password" name="re_pass">
+
+            <input type="button" value="Sign Up"/>
+        </form>
+        <p>By clicking the Sign Up button, you are agree to our </br>
+        <a href="#">Terms and Condition</a> and <a href="#">Privacy</a></p>
+        <p class="para-2">Already have an account? <a href="login.php">Login here</a></p> 
+    </div>
+    
 </body>
 </html>
